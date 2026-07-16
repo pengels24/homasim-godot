02c46c564d18956c7bc1d881e74957b090f5dbc6
Peter Engels <angelus2010@gmail.com>
2026-07-16 19:25:11 +0200

    chore: remove temp scripts

--- check_enc.py
+++
@@ -1,6 +0,0 @@
-﻿with open('scenes/ingame/hud/modals/content/cards/RowAssignedStaff.gd', 'rb') as f:
-    text = f.read().decode('utf-8')
-if 'Ã' in text:
-    print('RowAssignedStaff is double encoded!')
-else:
-    print('Clean!')

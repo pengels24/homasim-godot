b5114332d728a75f3c5123533ba6c4d390e0146f
Peter Engels <angelus2010@gmail.com>
2026-07-16 19:25:01 +0200

    fix(lang): Kaputte Umlaute (Double UTF-8 Encoding) in language.csv und ModalContentStaff.gd repariert

---
+++ check_enc.py
@@ -0,0 +1,6 @@
+﻿with open('scenes/ingame/hud/modals/content/cards/RowAssignedStaff.gd', 'rb') as f:
+    text = f.read().decode('utf-8')
+if 'Ã' in text:
+    print('RowAssignedStaff is double encoded!')
+else:
+    print('Clean!')
---
+++ fix_enc.py
@@ -0,0 +1,21 @@
+﻿import os
+
+def fix_file(filepath):
+    with open(filepath, 'rb') as f:
+        data = f.read()
+    
+    text = data.decode('utf-8')
+    replacements = {
+        'Ã¤': 'ä', 'Ã¶': 'ö', 'Ã¼': 'ü',
+        'Ã„': 'Ä', 'Ã–': 'Ö', 'Ãœ': 'Ü',
+        'ÃŸ': 'ß', 'Â´': '´'
+    }
+    
+    for k, v in replacements.items():
+        text = text.replace(k, v)
+        
+    with open(filepath, 'wb') as f:
+        f.write(text.encode('utf-8'))
+
+fix_file('scenes/ingame/hud/modals/content/ModalContentStaff.gd')
+fix_file('translations/language.csv')
